fb7ab2ac207f35508b5cb811068075e47250b8dc
Henry Tsai <henrytsai@outlook.com>
2020-11-16 18:04:05 -0800

    Exported more models.

--- lib/index.ts
+++ lib/index.ts
@@ -2,17 +2,25 @@
 
 // SDK exports.
 import IonDid from './IonDid';
+import IonDocumentModel from './models/IonDocumentModel';
 import IonKey from './IonKey';
+import IonNetwork from './enums/IonNetwork';
+import IonPublicKeyModel from './models/IonPublicKeyModel';
 import IonPublicKeyPurpose from './enums/IonPublicKeyPurpose';
 import IonRequest from './IonRequest';
 import IonSdkConfig from './IonSdkConfig';
 import IonServiceModel from './models/IonServiceModel';
+import JwkEs256k from './models/JwkEs256k';
 
 export {
-  IonPublicKeyPurpose,
   IonDid,
+  IonDocumentModel,
   IonKey,
+  IonNetwork,
+  IonPublicKeyModel,
+  IonPublicKeyPurpose,
   IonSdkConfig,
   IonServiceModel,
-  IonRequest
+  IonRequest,
+  JwkEs256k
 };
