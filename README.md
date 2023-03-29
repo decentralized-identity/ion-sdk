0132a688dab85ae2402b6a67046ba0d4d2b608a0
Hristo Kochev <h.l.kochev@gmail.com>
2023-03-29 14:32:51 +0300

    cjs ems with green tests

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 Code Coverage
 
-![Statements](https://img.shields.io/badge/statements-100%25-brightgreen.svg?style=flat) ![Branches](https://img.shields.io/badge/branches-100%25-brightgreen.svg?style=flat) ![Functions](https://img.shields.io/badge/functions-100%25-brightgreen.svg?style=flat) ![Lines](https://img.shields.io/badge/lines-100%25-brightgreen.svg?style=flat)
+![Statements](https://img.shields.io/badge/statements-Unknown%25-brightgreen.svg?style=flat) ![Branches](https://img.shields.io/badge/branches-Unknown%25-brightgreen.svg?style=flat) ![Functions](https://img.shields.io/badge/functions-Unknown%25-brightgreen.svg?style=flat) ![Lines](https://img.shields.io/badge/lines-Unknown%25-brightgreen.svg?style=flat)
 
 ```
 npm i @decentralized-identity/ion-sdk --save
--- tests/AdaDid.spec.ts
+++ tests/AdaDid.spec.ts
@@ -1,7 +1,7 @@
-import * as jwkEs256k1Public from './vectors/inputs/jwkEs256k1Public.json';
-import * as jwkEs256k2Public from './vectors/inputs/jwkEs256k2Public.json';
-import * as publicKeyModel1 from './vectors/inputs/publicKeyModel1.json';
-import * as service1 from './vectors/inputs/service1.json';
+import jwkEs256k1Public from './vectors/inputs/jwkEs256k1Public.json';
+import jwkEs256k2Public from './vectors/inputs/jwkEs256k2Public.json';
+import publicKeyModel1 from './vectors/inputs/publicKeyModel1.json';
+import service1 from './vectors/inputs/service1.json';
 import { AdaDid, DidKey, IonPublicKeyPurpose, IonSdkConfig } from '../lib/index';
 import AdaNetwork from '../lib/enums/AdaNetwork';
 import Encoder from '../lib/Encoder';
@@ -25,7 +25,7 @@ describe('AdaDid', async () => {
         publicKeys: didDocumentKeys,
         services
       };
-
+      // console.log(`recovery key: ${JSON.stringify(recoveryKey)} \n update key: ${JSON.stringify(updateKey)} document:  ${JSON.stringify(document)}`);
       const longFormDid = await AdaDid.createLongFormDid({ recoveryKey, updateKey, document });
 
       const expectedMethodSpecificId = 'did:ada:EiDyOQbbZAa3aiRzeCkV7LOx3SERjjH93EXoIM3UoN4oWg:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJwdWJsaWNLZXlNb2RlbDFJZCIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJ0WFNLQl9ydWJYUzdzQ2pYcXVwVkpFelRjVzNNc2ptRXZxMVlwWG45NlpnIiwieSI6ImRPaWNYcWJqRnhvR0otSzAtR0oxa0hZSnFpY19EX09NdVV3a1E3T2w2bmsifSwicHVycG9zZXMiOlsiYXV0aGVudGljYXRpb24iLCJrZXlBZ3JlZW1lbnQiXSwidHlwZSI6IkVjZHNhU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOSJ9XSwic2VydmljZXMiOlt7ImlkIjoic2VydmljZTFJZCIsInNlcnZpY2VFbmRwb2ludCI6Imh0dHA6Ly93d3cuc2VydmljZTEuY29tIiwidHlwZSI6InNlcnZpY2UxVHlwZSJ9XX19XSwidXBkYXRlQ29tbWl0bWVudCI6IkVpREtJa3dxTzY5SVBHM3BPbEhrZGI4Nm5ZdDBhTnhTSFp1MnItYmhFem5qZEEifSwic3VmZml4RGF0YSI6eyJkZWx0YUhhc2giOiJFaUNmRFdSbllsY0Q5RUdBM2RfNVoxQUh1LWlZcU1iSjluZmlxZHo1UzhWRGJnIiwicmVjb3ZlcnlDb21taXRtZW50IjoiRWlCZk9aZE10VTZPQnc4UGs4NzlRdFotMkotOUZiYmpTWnlvYUFfYnFENHpoQSJ9fQ';
